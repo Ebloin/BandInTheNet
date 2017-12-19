93db3717f6a7c9a3b9db6439c40bb08974954851
Ebloin <33060432+Ebloin@users.noreply.github.com>
2017-12-19 08:47:12 +0100

    Script registrazione canzoni su batteria

--- Script/batteria.js
+++ Script/batteria.js
@@ -11,6 +11,10 @@ var suoni = {
     90: 'bacchette'
 };
 
+var recording = false;
+var noteRegistrate= [];
+var indiceLoop=0;
+
 function init() {
     createjs.Sound.registerSound("Audio/AudioBatt/hi-hat.mp3", 'hi-hat');
     createjs.Sound.registerSound("Audio/AudioBatt/tom-tom.mp3", 'tom-tom');
@@ -24,13 +28,20 @@ function init() {
         
     document.addEventListener('keydown', premotasto);
     document.addEventListener('keyup', lasciotasto);
-
+    document.getElementById('play').addEventListener('click', playNote);
+    document.getElementById('recButton').addEventListener('click', recHandler);
+    document.getElementById('reset').addEventListener('click', resetRegistrata);
+    document.getElementById('salvaCanzone').addEventListener('click', salvaCanzone);
+    document.getElementById('undo').addEventListener('click', undo);
 };
 
 var premotasto = function(e) {
-
     var nota = suoni[e.keyCode];
     if (!nota) return;
+    if (recording) {
+        noteRegistrate.push(nota);
+        document.getElementById('noteregistrate').innerHTML = noteRegistrate;
+    }
     createjs.Sound.play(nota);
 };
 
@@ -38,3 +49,131 @@ var lasciotasto = function(e) {
     var nota = suoni[e.keyCode];
     if (!nota) return;
 };
+
+
+//Parte di registrazione
+var registra= function() {
+    recording = true;
+    //INSERIRE CONTROLLO SE VERAMENTE UOLE CANCELLARE
+    noteRegistrate = [];
+    $('#noteregistrate').text('');
+};
+
+var stopRegistra= function() {recording = false};
+
+var playNote= function() {
+    if (noteRegistrate.length == 0) {
+        alert('Spiacenti ma non hai ancora registrato nessuna melodia, prova con il pulsante rosso');
+        return;
+    }
+    var loop = setInterval(function() {
+        nota= noteRegistrate[indiceLoop];
+        indiceLoop++;    
+        suona(nota);
+        if (indiceLoop == noteRegistrate.length) {
+            indiceLoop=0;
+            clearInterval(loop);
+        }
+    }, 500);
+}
+
+var suona= function(nota) {
+    createjs.Sound.play(nota);
+}
+
+var recHandler= function(e) {
+    var button= e.target;
+    var songBar= document.getElementById('registrata');
+    //non sto registrando, inizia a registrare
+    if (button.classList.contains('notRec')){
+        button.classList.replace('notRec', 'Rec');
+        songBar.classList.replace('nonVisibile', 'visibile');
+        recording = true;
+        //INSERIRE CONTROLLO SE VERAMENTE UOLE CANCELLARE
+        noteRegistrate = [];
+        $('#noteregistrate').text('');
+    }
+
+    //Sto registrando, stop alla registrazione
+    else if (button.classList.contains('Rec')){
+        button.classList.replace('Rec', 'notRec');
+        recording = false;
+        if (noteRegistrate.length == 0) {
+            $('#registrata').switchClass('visibile', 'nonVisibile');
+        }
+    }
+}
+
+var resetRegistrata= function() {
+    noteRegistrate= [];  
+    if($('#recButton').hasClass('notRec')) {
+        $('#registrata').switchClass('visibile', 'nonVisibile');        
+        $('#noteregistrate').html(noteRegistrate);
+        alert('Le note registrate sono state resettate');
+    }
+    if($('#recButton').hasClass('Rec')) {  
+        $('#noteregistrate').html(noteRegistrate);
+        alert('Le note registrate sono state resettate');
+    }
+}
+
+var salvaCanzone= function() {
+    //Conrollo sul nome della canzone
+    if ($('#nomeCanzone').val() == '') {
+        return;
+    }
+    //Controllo utente loggato
+    if (localStorage.utenteCorrente == undefined || localStorage.utenteCorrente == '') {
+        alert('Per salvare una canzone devi prima loggarti');
+        return;
+    }
+    var nomeUtente= localStorage.utenteCorrente;
+    var arrayUsers= JSON.parse(localStorage.utenti);
+    //Calcolo indice utente
+    var index;
+    for (i=0; i<arrayUsers.length; i++) {
+        if (JSON.stringify(arrayUsers[i].Username) == JSON.stringify(nomeUtente)) {
+            index= i;
+        }
+    }
+    
+    //Controllo canzone già presente
+    if (arrayUsers[index].Songs.map(function(x) {return x.nome; }).indexOf($('#nomeCanzone').val()) != -1) {
+        var sostituisci = confirm("E'già presente una canzone con questo nome, vuoi sovrascriverla?");
+        if (!sostituisci) {
+            return;
+        }
+        var indiceCanzone= arrayUsers[index].Songs.map(function(x) {return x.nome; }).indexOf($('#nomeCanzone').val());
+        arrayUsers[index].Songs[indiceCanzone].note = noteRegistrate;
+        alert('"'+ $('#nomeCanzone').val() +'" aggiornata con successo');
+        $('#nomeCanzone').val('');
+        $('#registrata').switchClass('visibile', 'nonVisibile');
+        if ($('#recButton').hasClass('Rec')) {
+            $('#recButton').switchClass('Rec', 'notRec');
+            recording = false;
+        }
+        return;
+    }
+
+    //Creo oggetto canzone
+    var canzone = {
+        nome: $('#nomeCanzone').val(),
+        note: noteRegistrate
+    }
+    
+    arrayUsers[index].Songs.push(canzone);
+    var prova= arrayUsers[index].Songs;
+    localStorage.utenti= JSON.stringify(arrayUsers);
+    alert('Canzone aggiunta al tuo profilo');
+    $('#nomeCanzone').val('');
+    $('#registrata').switchClass('visibile', 'nonVisibile');
+    if ($('#recButton').hasClass('Rec')) {
+        $('#recButton').switchClass('Rec', 'notRec');
+        recording = false;
+    }
+}
+
+var undo= function() {
+    noteRegistrate.pop();
+    document.getElementById('noteregistrate').innerHTML = noteRegistrate;
+}
